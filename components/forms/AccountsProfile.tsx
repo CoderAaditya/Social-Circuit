172b57322db7ec072133c313f8c8dd7ee4191164
CoderAaditya <aadityathakurcs@gmail.com>
2024-12-17 01:05:54 +0530

    Changes In AccountProfile Page

--- components/forms/AccountsProfile.tsx
+++ components/forms/AccountsProfile.tsx
@@ -75,8 +75,8 @@ function AccountsProfile({ user, btnTitle }: Props) {
     const hasImageChanged = isBase64Image(blob);
     if (hasImageChanged) {
       const imgRes = await startUpload(files);
-      if (imgRes && imgRes[0].fileUrl) {
-        values.profile_photo = imgRes[0].fileUrl;
+      if (imgRes && imgRes[0].url) {
+        values.profile_photo = imgRes[0].url;
       }
     }
 
